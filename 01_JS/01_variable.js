f9866a7168e8a1fd0a3f3a02e8dc857fc0fda344
Sunnyshivam123 <117255299+Sunnyshivam123@users.noreply.github.com>
2023-11-03 11:22:20 +0000

    Learning Variable

---
+++ 01_JS/01_variable.js
@@ -0,0 +1,18 @@
+const accId=12345;
+let accEmail="sunny@google.com";
+var accPassword="abc123";
+accCity="Banglore";
+
+// accId=2;  Not Allowed
+accEmail="shivam@google.com";
+accPassword="kpmg123";
+accCity="Hyderabad";
+
+let accState;
+
+console.log([accId, accEmail, accPassword,accCity,accState]);
+
+/*
+Prefer not to use Var because of issue in Block scope and Functional scope
+*/
+
