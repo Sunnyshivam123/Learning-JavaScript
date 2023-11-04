26e06dfa1e6aed8e6729cb6a3f4717c350e3a813
Sunnyshivam123 <117255299+Sunnyshivam123@users.noreply.github.com>
2023-11-04 15:48:10 +0000

    dataTypes

---
+++ 01_JS/02_dataTypes.js
@@ -0,0 +1,10 @@
+//number=>2 to power 53
+//bigInt=>
+//null=>Standalone value
+//undefined=>means value is not assigned yet
+//boolean=>true/false
+//symbol=>unique
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
