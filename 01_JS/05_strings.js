e8a75e42caebcdfaa247a0e4edb2535332270071
Sunnyshivam123 <117255299+Sunnyshivam123@users.noreply.github.com>
2023-11-07 09:37:55 +0000

    string

---
+++ 01_JS/05_strings.js
@@ -0,0 +1,10 @@
+let url="https://www.sks%20@gmail.com";
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('sks'));
+console.log(url.includes('kumar'));
+console.log(url.lastIndexOf('t'));
+
+let name="Shivam";
+let age=24;
+console.log(`My Name is ${name} and age is ${age}`);
