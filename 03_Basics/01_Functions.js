6642c5d5c58c16cb1c2f37f4a0812af5ced63626
Sunnyshivam123 <117255299+Sunnyshivam123@users.noreply.github.com>
2023-11-10 17:47:51 +0000

    function basic

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,30 @@
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+// addTwoNumbers(3,4);
+// let answer=addTwoNumbers(3,4);
+// console.log(answer);
+
+function addTwoNumbers(number1,number2){
+    // let result=number1+number2;
+    // return result;
+    return number1+number2;
+}
+let answer=addTwoNumbers(3,4);
+    // console.log(answer);
+
+function userLoggedIn(userName){
+    return `${userName} is logged in`;
+}
+// console.log(userLoggedIn("Sunny"))
+console.log(userLoggedIn(""))// is logged in
+console.log(userLoggedIn())// undefined is logged in
+
+
+
+
+
+
+
+
+
