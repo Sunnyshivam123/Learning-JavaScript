aa9d3fbcc159f46e02e31c64b6d888f4d6f66bba
Sunnyshivam123 <117255299+Sunnyshivam123@users.noreply.github.com>
2023-11-18 08:11:38 +0000

    Loops

---
+++ 04_Iterations/one.js
@@ -0,0 +1,24 @@
+//for
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    
+}
+
+let myArray=["Ram","Shyam","Ratanesh"];
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+//While Loop
+
+let array1=['kkr','rbc','mi','csk'];
+let arr=0;
+
+while (arr<array1.length) {
+    console.log(array1[arr]);
+    arr++;
+}
---
+++ 04_Iterations/two.js
@@ -0,0 +1,21 @@
+let array1=[1,2,3,4,5];
+
+for (const val of array1) {
+    console.log(val);
+}
+
+let str="Hello World!";
+
+for (const char of str) {
+    console.log(char);
+}
+
+//Map
+
+const map = new Map();
+map.set("IN","india");
+map.set("USA","United states of america");
+map.set("AS","Australia");
+map.set("UK","United Kingdom");
+
+console.log(map);
